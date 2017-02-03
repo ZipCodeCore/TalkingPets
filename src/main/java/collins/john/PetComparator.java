97f8554f4b75765fb813e3ddbfd73131e30792cd
John Collins <johncollins@zipcoders-MacBook-Pro-4.local>
2017-02-03 00:47:41 -0500

    updated UML forComparator

---
+++ src/main/java/collins/john/PetComparator.java
@@ -0,0 +1,12 @@
+package collins.john;
+
+import java.util.Comparator;
+
+/**
+ * Created by johncollins on 2/3/17.
+ */
+public class PetComparator implements Comparator<Pet> {
+    public int compare(Pet o1, Pet o2) {
+        return 0;
+    }
+}
