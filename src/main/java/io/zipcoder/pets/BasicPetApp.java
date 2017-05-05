4aaa7ca2afd0349f83ac5fed9d41aa005c1bdf06
Dan Zygmund-Felt <danzygmund-felt@zipcoder10.local>
2017-05-05 17:13:57 -0400

    Added UML and split some methods out of BasicPetApp.main

--- src/main/java/io/zipcoder/pets/BasicPetApp.java
+++ src/main/java/io/zipcoder/pets/BasicPetApp.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 
 public class BasicPetApp {
 
-    static Pet petSelector(String pet) {
+    private static Pet petSelector(String pet) throws Exception{
         if(pet.equalsIgnoreCase("Cat")) {
             Cat cat = new Cat();
             return cat;
@@ -18,34 +18,37 @@ public class BasicPetApp {
         } else if(pet.equalsIgnoreCase("Goldfish")){
             Goldfish goldfish = new Goldfish();
             return goldfish;
-        } else {
-            return null;
         }
+        return null;
     }
 
-    static void printPets(Pet[] pets) {
+    private static void printPets(Pet[] pets) {
         for (Pet p : pets) {
             System.out.println(p.getName() + " says " + p.speak() + "\n");
         }
     }
 
 
+    private static Pet[] petInput() throws Exception{
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("How many pets you got?");
+        Pet[] petList = new Pet[Integer.parseInt(scanner.nextLine())];
+        for (int i = 0; i < petList.length; i++) {
+            System.out.println("What kind of pet is pet number " + (i + 1) + "?");
+            Pet pet = petSelector(scanner.nextLine());
+            System.out.println("What's your pet's name?");
+            pet.setName(scanner.nextLine());
+            petList[i] = pet;
+        }
+        return petList;
+    }
+
 
     public static void main(String[] args) {
 
         try {
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("How many pets you got?");
-            Pet[] petList = new Pet[Integer.parseInt(scanner.nextLine())];
-            for (int i = 0; i < petList.length; i++) {
-                System.out.println("What kind of pet is pet number " + (i + 1) + "?");
-                Pet pet = petSelector(scanner.nextLine());
-                System.out.println("What's your pet's name?");
-                pet.setName(scanner.nextLine());
-                petList[i] = pet;
-            }
 
-            System.out.println();
+            Pet[] petList = petInput();
 
             Arrays.sort(petList);
             printPets(petList);
@@ -55,7 +58,9 @@ public class BasicPetApp {
 
 
         } catch (Exception e) {
+
             System.out.println("That's not an acceptable pet. I'm done with you.");
+
         }
 
     }
