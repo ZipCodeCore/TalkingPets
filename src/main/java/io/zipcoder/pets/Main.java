126f8a9c0f00bee2e06f11fd7ad5cf42a1884bf8
gabriela <gabriela@zipcoders-MacBook-Pro-6.local>
2017-11-06 11:33:19 -0500

    Implementation for Pets/Dog/Cat/Fish no tests.

---
+++ src/main/java/io/zipcoder/pets/Main.java
@@ -0,0 +1,17 @@
+package io.zipcoder.pets;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("How many pets do you have?");
+        int numOfPets = sc.nextInt();
+
+
+
+    }
+
+}
