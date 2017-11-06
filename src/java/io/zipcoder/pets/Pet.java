99b9532d447f87fd7be18a174bf86cadee5e9c59
ryan <ryan@zipcoders-MacBook-Pro-5.local>
2017-11-06 12:17:35 -0500

    once again

--- src/java/io/zipcoder/pets/Pet.java
+++ src/java/io/zipcoder/pets/Pet.java
@@ -1,4 +1,23 @@
 package io.zipcoder.pets;
 
 public class Pet {
+    private String name;
+
+    public Pet(String name){
+        this.name = name;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void speak(){
+
+    }
+
 }
--- src/test/io/zipcoder/pets/PetTest.java
+++ src/test/io/zipcoder/pets/PetTest.java
@@ -1,4 +1,30 @@
 package io.zipcoder.pets;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class PetTest {
+    Pet pet = new Pet("buck");
+
+    @Test
+    public void testConstructor(){
+        String expected = "buck";
+        String actual = pet.getName();
+        Assert.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void testSetName(){
+        String expected = "barry";
+        pet.setName("barry");
+        String actual = pet.getName();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSpeak(){
+
+    }
+
 }
