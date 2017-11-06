c4f14f15f9eb06382c4b35b4b38d93604132244e
andrea <andrea@zipcoders-MacBook-Pro-2.local>
2017-11-06 14:58:38 -0500

    deleted petstest updated dog and otter and created dog test and otter test

--- src/main/java/io/zipcoder/polymorphism/Otter.java
+++ src/main/java/io/zipcoder/polymorphism/Otter.java
@@ -1,4 +1,6 @@
 package io.zipcoder.polymorphism;
 
 public class Otter {
+    public String speak() {
+    }
 }
--- src/test/java/io/zipcoder/polymorphism/DogTest.java
+++ src/test/java/io/zipcoder/polymorphism/DogTest.java
@@ -1,4 +1,15 @@
 package io.zipcoder.polymorphism;
 
+import org.junit.Test;
+import org.junit.Assert;
+
 public class DogTest {
+
+    @Test
+    public void testDogSpeak(){
+        Dog dog = new Dog();
+        String expected = "Woof";
+        String actual = dog.speak();
+        Assert.assertEquals(expected, actual);
+    }
 }
