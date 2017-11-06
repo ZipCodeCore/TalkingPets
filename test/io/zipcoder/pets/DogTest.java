2da1f75f1e5dc36c2c7c9ab2a610acec15393844
gpatelos <greg.patselas@gmail.com>
2017-11-06 13:24:50 -0500

    all tests

--- test/io/zipcoder/pets/DogTest.java
+++ test/io/zipcoder/pets/DogTest.java
@@ -1,5 +1,43 @@
+package io.zipcoder.pets;
+
+import org.junit.Assert;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class DogTest {
+
+    @Test
+    public void setNameTest(){
+        Dog dog = new Dog();
+        String expected = "Maude";
+
+        dog.setName("Maude");
+        String actual = dog.getName();
+
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void getNameTest(){
+        Dog dog = new Dog("Buddy");
+
+        String expected = "Buddy";
+        String actual = dog.getName();
+
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void speakTest(){
+        Dog dog = new Dog();
+
+        String expected = "Ruff.";
+
+        String actual = dog.speak();
+
+        Assert.assertEquals(expected,actual);
+
+    }
 
 }
--- test/io/zipcoder/pets/PetTest.java
+++ test/io/zipcoder/pets/PetTest.java
@@ -1,5 +1,48 @@
+package io.zipcoder.pets;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.rmi.MarshalException;
+
 import static org.junit.Assert.*;
 
 public class PetTest {
 
+    @Test
+    public void getNameTest(){
+        Pet pet = new Pet();
+        String expected = "Russel";
+
+        pet.setName("Russel");
+        String actual = pet.getName();
+
+        Assert.assertEquals(expected,actual);
+
+    }
+
+    @Test
+    public void setNameTest(){
+        Pet pet = new Pet();
+        String expected = "Marlowe";
+
+        pet.setName("Joe");
+        pet.setName("Marlowe");
+
+        String actual = "Marlowe";
+
+        Assert.assertEquals(expected,actual);
+
+    }
+
+    @Test
+    public void speakTest(){
+        Pet pet = new Pet();
+
+        String expected = "I'm not your 'buddy,' pal.";
+        String actual = pet.speak();
+
+        Assert.assertEquals(expected,actual);
+
+    }
 }
