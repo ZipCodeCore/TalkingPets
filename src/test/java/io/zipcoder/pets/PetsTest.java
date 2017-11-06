2748360db996831b8e507059987994bec20d1ced
timothy <timothy@zipcoders-MacBook-Pro.local>
2017-11-06 12:11:40 -0500

    Tests for Pet class and subclasses

--- src/test/java/io/zipcoder/pets/PetsTest.java
+++ src/test/java/io/zipcoder/pets/PetsTest.java
@@ -10,26 +10,40 @@ import org.junit.Assert;
  */
 public class PetsTest {
 
-
-    @Test
-    public void testSetGetPetType(){
-        Pet pet = new Pet();
-        pet.setType("Snake");
-        String expected="Snake";
-        String actual=pet.getType();
-        Assert.assertTrue(expected.equals(actual));
-    }
-
     @Test
     public void testSetGetPetName(){
 
-        Pet pet = new Pet();
-        pet.setName("Nagini");
+        Pet nagini = new Snake();
+        nagini.setName("Nagini");
         String expected="Nagini";
-        String actual=pet.getName();
+        String actual=nagini.getName();
         Assert.assertTrue(expected.equals(actual));
 
     }
 
+    @Test
+    public void testTypeOfPet(){
+        Pet nagini = new Snake();
+        Pet krypto = new Dog();
+        Pet luna = new Cat();
+
+        Assert.assertTrue (nagini instanceof Snake && nagini instanceof Pet);
+        Assert.assertTrue (krypto instanceof Dog && krypto instanceof Pet);
+        Assert.assertTrue (luna instanceof Cat && luna instanceof Pet);
+        Assert.assertTrue("Snake".equals(nagini.getClass().getName()));
+        Assert.assertTrue("Dog".equals(krypto.getClass().getName()));
+        Assert.assertTrue("Cat".equals(luna.getClass().getName()));
+    }
+
+    @Test
+    public void testSpeak(){
+        Pet nagini = new Snake();
+        Pet krypto = new Dog();
+        Pet luna = new Cat();
+
+        Assert.assertTrue("Parseltongue".equals(nagini.speak()));
+        Assert.assertTrue("Woof".equals(krypto.speak()));
+        Assert.assertTrue("Meow".equals(luna.speak()));
+    }
 
 }
--- src/test/java/io/zipcoder/polymorphism/MainApplicationTest.java
+++ src/test/java/io/zipcoder/polymorphism/MainApplicationTest.java
@@ -5,18 +5,8 @@ import org.junit.Test;
 
 
 /**
- * Created by leon on 11/6/17.
+ * Created by Timothy R Rager on 11/6/17.
  */
 public class MainApplicationTest {
-
-    @Test
-    public void testSetGetNumberOfPets(){
-
-    }
-
-    @Test
-    public void testPrintUserInput(){
-
-    }
 
 }
