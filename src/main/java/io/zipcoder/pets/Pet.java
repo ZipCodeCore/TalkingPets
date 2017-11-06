5c8fab6fdd8b6834f0b083e588712982860a5b52
timothy <timothy@zipcoders-MacBook-Pro.local>
2017-11-06 14:09:07 -0500

    Work on pet class

--- src/main/java/io/zipcoder/pets/Pet.java
+++ src/main/java/io/zipcoder/pets/Pet.java
@@ -1,4 +1,18 @@
 package io.zipcoder.pets;
 
-public class Pet {
+public abstract class Pet {
+
+    private String name;
+    private String speech;
+
+    public Pet(String name){
+        this.name=name;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public void setName
+
 }
