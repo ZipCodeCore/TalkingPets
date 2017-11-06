eeb1d7039e9bfc43157d03ab3b89b1df584969c9
rikeroleg <rikeroleg@gmail.com>
2017-11-06 12:23:53 -0500

    constructor

--- src/main/java/io/zipcoder/polymorphism/Pet.java
+++ src/main/java/io/zipcoder/polymorphism/Pet.java
@@ -4,10 +4,27 @@ public abstract class Pet {
     static String name;
     static String type;
 
+    public Pet() {}
+
     public String speak(String type , String speech){
         return ( name + " is a " + type +  " and it goes " + speech + " !");
     }
 
+    public static String getName() {
+        return name;
+    }
 
-    
+    public static void setName(String name) {
+        Pet.name = name;
+    }
+
+    public static String getType() {
+        return type;
+    }
+
+    public static void setType(String type) {
+        Pet.type = type;
+    }
+
+
 }
