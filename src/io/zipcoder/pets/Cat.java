1d258e295d96f3526b1214952fa1a6bb6a3ee81e
gpatelos <greg.patselas@gmail.com>
2017-11-06 14:07:23 -0500

    oops

--- src/io/zipcoder/pets/Cat.java
+++ src/io/zipcoder/pets/Cat.java
@@ -2,15 +2,13 @@ package io.zipcoder.pets;
 
 public class Cat extends Pet {
 
-    private String name = "";
 
     public Cat() {
 
     }
 
     public Cat(String name) {
-        super();
-        this.name = name;
+        super(name);
     }
 
 
