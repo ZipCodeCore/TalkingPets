83d45e7817d88c27adb6a7301cf996579d076510
jeffrey <jeffrey@zipcoders-MacBook-Pro-3.local>
2017-11-06 13:52:29 -0500

    Added pet  classes

---
+++ src/java/io/zipcoder/pets/Main.java
@@ -0,0 +1,9 @@
+package io.zipcoder.pets;
+
+import java.util.Scanner;
+
+public class Main {
+
+
+
+}
---
+++ src/java/io/zipcoder/pets/Pet.java
@@ -0,0 +1,19 @@
+package io.zipcoder.pets;
+
+public class Pet {
+
+    public String name;
+
+    public String Speak(String speak){
+        return  null;
+    }
+
+    public Pet(String name){
+        this.name=name;
+           }
+
+    public String setName(){return this.name;}
+    public String getName(){return name;}
+
+
+}
