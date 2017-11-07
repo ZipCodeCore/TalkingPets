b5a8367a6517c1533ef94dc89cb61fe0b57f9a17
samuel <samuel@zipcoders-MacBook-Pro.local>
2017-11-07 13:42:20 -0500

    final Interfaces

--- src/java/io/zipcoder/pets/Pet.java
+++ src/java/io/zipcoder/pets/Pet.java
@@ -12,19 +12,20 @@ public class Pet implements Comparable<Pet> {
     }
 
     public Pet(String name) {
+
         this.name = name;
     }
 
-    public String setName() {
+    public String setName() {         //tested
         return this.name;
     }
 
-    public String getName() {
+    public String getName() {          //tested
         return name;
     }
 
     @Override
-    public int compareTo(Pet o) {
+    public int compareTo(Pet o) {             //tested
         if(this.getName().compareTo(o.getName())==0){
             return this.getClass().getName().compareTo(o.getClass().getName());
         } else {
--- src/test/java/io/zipcoder/pets/PetTest.java
+++ src/test/java/io/zipcoder/pets/PetTest.java
@@ -1,24 +1,49 @@
 package io.zipcoder.pets;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class PetTest {
+    Cat Raisin = new Cat("Raisin");
+
     @Test
-    public void speak() throws Exception {
+    public void testSpeak()  {
+        String expected = "meow.";
+        String actual = Raisin.Speak();
+        Assert.assertEquals(expected,actual);
+
+
     }
 
     @Test
-    public void setName() throws Exception {
+    public void setName()  {
+        Pet name = new Pet("Jeff");
+        String expected = "Jeff";
+        String actual = name.setName();
+        Assert.assertEquals(expected,actual);
+
     }
 
     @Test
-    public void getName() throws Exception {
+    public void getName()  {
+        Pet name = new Pet("Sam");
+        String expected = "Sam";
+        String actual = name.getName();
+        Assert.assertEquals(expected,actual);
+
     }
 
     @Test
-    public void compareTo() throws Exception {
+    public void compareTo()  {
+
+        int[] list1 = {1,2};
+        int[] list2 = {1,2};
+
+        Assert.assertArrayEquals(list1, list2);
+
+
     }
 
 }
