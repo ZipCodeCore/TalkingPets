8d3b0c9932978069f6f92b196672a968697bbe3c
donnasvinis <dvsvinis@mac.com>
2019-06-06 15:18:54 -0400

    Initial Commit. Part 1 completed.

--- src/main/java/io/zipcoder/polymorphism/MainApplication.java
+++ src/main/java/io/zipcoder/polymorphism/MainApplication.java
@@ -1,7 +1,39 @@
 package io.zipcoder.polymorphism;
 
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
 /**
  * Created by leon on 11/6/17.
  */
 public class MainApplication {
+
+    public static void main(String[] args) {
+
+        ArrayList<String> userInput = new ArrayList<String>();
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("How many pets do you have: ");
+        String numberOfPets = input.nextLine();
+        userInput.add(numberOfPets);
+
+        for (int i = 1; i <= Integer.valueOf(numberOfPets); i++){
+            System.out.print("What type of pet is pet # " + i + ": ");
+            userInput.add(input.nextLine());
+            System.out.print("What is the name of pet # " + i) + ": ";
+            userInput.add(input.nextLine());
+        }
+
+        System.out.println("Thank you.");
+        System.out.println(Arrays.toString(userInput.toArray()));
+
+
+    }
+
+
+
 }
+
+
