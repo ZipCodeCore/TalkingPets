4da8e81d0b1db541d050012fc3c55ec7aa81646e
April Howard <ahoward@zipcodes-MBP-5.fios-router.home>
2020-03-01 16:18:46 -0500

    add cat, dog and hampster class and main app questions

---
+++ src/main/java/io/zipcoder/pets/Pet.java
@@ -0,0 +1,34 @@
+package io.zipcoder.pets;
+
+import io.zipcoder.polymorphism.Animal;
+
+public abstract class Pet implements Animal {
+    private String name;
+    private String type;
+    protected Pet() {}
+
+   public Pet(String type, String name) {
+        this.type = type;
+        this.name = name;
+   }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String speak() {
+        return "";
+    }
+}
--- src/main/java/io/zipcoder/polymorphism/MainApplication.java
+++ src/main/java/io/zipcoder/polymorphism/MainApplication.java
@@ -1,7 +1,46 @@
 package io.zipcoder.polymorphism;
 
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+import io.zipcoder.pets.Cat;
+import io.zipcoder.pets.Dog;
+import io.zipcoder.pets.Hampster;
+import io.zipcoder.pets.Pet;
+
+
+import javax.crypto.spec.PSource;
+import javax.xml.namespace.QName;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  * Created by leon on 11/6/17.
  */
 public class MainApplication {
+
+    public static void main(String[] args) {
+        String petType, petName;
+        ArrayList<Pet> petArrayList = new ArrayList<Pet>();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("How many pets do you have?");
+        int numberOfPets = scanner.nextInt();
+
+        for(int i = 1; i <= numberOfPets; i++) {
+            System.out.println("What kind of pet?");
+            petType = scanner.next();
+            System.out.println("Enter pet's name.");
+            petName = scanner.next();
+            if ("dog".equals(petType.toLowerCase())) {
+                petArrayList.add(new Dog(petType, petName));
+            } else if ("cat".equals(petType.toLowerCase())) {
+                petArrayList.add(new Cat(petType, petName));
+            } else if ("hampster".equals(petType.toLowerCase())) {
+                petArrayList.add(new Hampster(petType, petName));
+            }
+        }
+        for (Pet petObj : petArrayList) {
+            System.out.println(petObj.getName() + " " + petObj.speak());
+
+        }
+    }
 }
