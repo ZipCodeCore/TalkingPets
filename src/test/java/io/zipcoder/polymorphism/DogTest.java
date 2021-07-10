238346cfe15400b9289573de172988252747e192
Nathan <nathan@zipcodes-MacBook-Pro-9.local>
2021-07-10 13:02:02 -0400

    started new dog test

---
+++ src/test/java/io/zipcoder/polymorphism/DogTest.java
@@ -0,0 +1,13 @@
+package io.zipcoder.polymorphism;
+
+import org.junit.Test;
+
+public class DogTest {
+
+    @Test
+    public void dogTest(){
+        //given
+
+        Dog dog = new Dog();
+    }
+}
