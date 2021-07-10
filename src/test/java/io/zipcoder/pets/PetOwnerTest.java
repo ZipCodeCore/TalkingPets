4e2af0c349664abd00778dec85ac1a52d2f8146a
jgiroso <jrgiroso@gmail.com>
2021-07-10 17:20:21 -0400

    added test classes and tests

---
+++ src/test/java/io/zipcoder/pets/PetOwnerTest.java
@@ -0,0 +1,74 @@
+package io.zipcoder.pets;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class PetOwnerTest {
+
+    @Test
+    public void testPetOwnerConstructor() {
+        //given
+        String expectedName = "Kira";
+        //when
+        PetOwner petOwner = new PetOwner(expectedName);
+        String actualName = petOwner.getName();
+        //then
+        Assert.assertEquals(expectedName, actualName);
+    }
+
+    @Test
+    public void testSetNumberOfPets() {
+        //given
+        int expected = 1;
+        PetOwner petOwner = new PetOwner("Annette");
+        //when
+        petOwner.setNumberOfPets(expected);
+        int actual = petOwner.setNumberOfPets();
+
+        //then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testAddPets() {
+        //given
+        Pet[] expectedPets = {cat1, cat2};
+        PetOwner petOwner = new PetOwner("Caroline");
+        petOwner.setNumberOfPets(2);
+        Cat cat1 = new Cat("Luna", 0);
+        Cat cat2 = new Cat("Binks", 1);
+        //when
+        petOwner.addPet(cat1);
+        petOwner.addPet(cat2);
+        Pet[] actualPets = petOwner.getPets();
+        //then
+        Assert.assertEquals(expectedPets, actualPets);
+    }
+
+    @Test
+    public void testGetPetById() {
+        //given
+        String expected = "YunYun";
+        PetOwner petOwner = new PetOwner("Rayna");
+        Dog dog = new Dog(expected, 0);
+        Cat cat = new Cat("Sophie", 1);
+        //when
+        String actual = petOwner.getPetById(0);
+        //then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSetPetName() {
+        //given
+        String expected = "Poppy";
+        PetOwner petOwner = new PetOwner("Peter");
+        petOwner.setNumberOfPets(2);
+        Cat cat = new Cat("Ghost", 0);
+        //when
+        petOwner.setPetName(cat, expected);
+        String actual = cat.getName();
+        //then
+        Assert.assertEquals(expected, actual);
+    }
+}
