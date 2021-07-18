cce9a4a9fe2acd51268a19f95cbccdbf8e1ca053
Dee <dee@zipcodes-MacBook-Pro-2.local>
2021-07-17 22:41:30 -0400

    tests and print in main

--- src/test/java/io/zipcoder/polymorphism/MainApplicationTest.java
+++ src/test/java/io/zipcoder/polymorphism/MainApplicationTest.java
@@ -75,4 +75,64 @@ public class MainApplicationTest {
         //then
         Assert.assertEquals(expected, actual);
     }
+
+    @Test
+    public void printDogTest(){
+        //given
+        String name = "Bronco";
+        MainApplication app = new MainApplication();
+        String expected = "Woof woof!";
+
+        //when
+        app.addDog(name);
+        String petName = app.petList.get(0).getName();
+        String sound = app.petList.get(0).speak();
+
+        //then
+        Assert.assertEquals(name, petName);
+        Assert.assertEquals(expected, sound);
+
+    }
+
+    @Test
+    public void printDogAndCatTest(){
+        //given
+        String name = "Bronco";
+        String catName = "Persia";
+        MainApplication app = new MainApplication();
+        String expected = "Purrrr";
+
+        //when
+        app.addDog(name);
+        app.addCat(catName);
+        String petName = app.petList.get(1).getName();
+        String sound = app.petList.get(1).speak();
+
+        //then
+        Assert.assertEquals(catName, petName);
+        Assert.assertEquals(expected, sound);
+
+    }
+
+    @Test
+    public void printParrotTest(){
+        //given
+        String name = "Bronco";
+        String catName = "Persia";
+        String parrotName = "Charlie";
+        MainApplication app = new MainApplication();
+        String expected = "I do what I want";
+
+        //when
+        app.addDog(name);
+        app.addCat(catName);
+        app.addParrot(parrotName);
+        String petName = app.petList.get(2).getName();
+        String sound = app.petList.get(2).speak();
+
+        //then
+        Assert.assertEquals(parrotName, petName);
+        Assert.assertEquals(expected, sound);
+
+    }
 }
